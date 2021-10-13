6d9ef5bbcbacf41306c212dde82f6f2f7223bfc1
adamchappell00 <adam.chappell00@gmail.com>
2021-10-13 11:49:33 -0500

    functions completed and tested

--- solutions.js
+++ solutions.js
@@ -9,14 +9,64 @@
  * If you don't know what an iffe is, don't worry about it :)
  *
  * This file is for defining the following functions. See README.md for more detail.
- * isTrue
- * isFalse
- * not
- * addOne
- * isEven
- * isIdentical
- * isEqual
- * or
- * and
- * concat
+ * isTrue: COMPLETE Ln 23
+ * isFalse: COMPLETE Ln 30
+ * not: COMPLETE Ln 37
+ * addOne: Complete Ln 40
+ * isEven: Complete Ln 43
+ * isIdentical: Complete Ln 50
+ * isEqual: Complete Ln 57
+ * or: Complete Ln 64
+ * and: Complete Ln 67
+ * concat: Complete Ln 70
  */
+function isTrue(input) {
+    if (input === true){
+        return true;
+    } else {
+        return false;
+    }
+}
+function isFalse(input) {
+    if (input === false){
+        return true;
+    } else {
+        return false;
+    }
+}
+function not(input){
+    return !input;
+}
+function addOne(number){
+    return (parseFloat(number) + 1);
+}
+function isEven(number){
+    if(number % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+function isIdentical(a,b){
+    if(a === b){
+        return true;
+    } else {
+        return false;
+    }
+}
+function isEqual(a,b){
+    if(a == b){
+        return true;
+    } else {
+        return false;
+    }
+}
+function or(a,b){
+    return a || b;
+}
+function and(a,b){
+    return a && b;
+}
+function concat(a,b){
+    return a.toString() + b.toString();
+}
